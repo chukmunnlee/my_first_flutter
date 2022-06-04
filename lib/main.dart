0972d11df3a19eec725301f902e504f9d35a1319
chukmunnlee <chukmunnlee@gmail.com>
2022-06-04 14:55:30 +0800

    handling button pressed

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('\n\nhello'),
+    return MaterialApp(
+        home: Scaffold( appBar: AppBar( title: const Text('My App'),),
+        //body: const Text('hello, world', textScaleFactor: 3,),
+        body: createQuestions('别问我是谁', 4),
+    ));
+  }
+
+  Column createQuestions(String text, int ans) {
+    return Column(
+      children: [
+        Text(text, textScaleFactor: 2,),
+        for (var i = 0; i < ans; i++)
+          ElevatedButton(
+            child: Text('Answer $i', textScaleFactor: 1.5,),
+            onPressed: () => print('Pressed $i'),
+          )
+      ],
     );
   }
 }
